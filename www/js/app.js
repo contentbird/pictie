37a77ac9dd1cc7a84edadd1cd78629b038b4b27d
nna <nico.nardone@gmail.com>
2014-07-16 16:37:01 +0200

    post text message to server and display response

--- www/js/app.js
+++ www/js/app.js
@@ -8,10 +8,47 @@
       this.newNumber = null;
     };
     this.logout = function(){
-      this.user = {};
+      this.user.number = null;
     };
   });
 
+  app.controller('MessageController', ['$http', function($http) {
+    var messageController = this;
+
+    this.user = user;
+    this.newMessage = {};
+    this.inbox  = inbox;
+    this.outbox = outbox;
+
+    this.canPost = function(){
+      return this.user.number && this.user.number.length
+    };
+
+    this.messageSent = function(data){
+      messageController.outbox.push(data.message);
+      messageController.newMessage = {};
+    };
+
+    this.sendMessage = function(){
+      $http.post('http://localhost:5000/messages', { sender: this.user.number,
+                                                     recipient: this.newMessage.recipient,
+                                                     body: this.newMessage.body}).
+            success(this.messageSent).
+            error(function(data){
+              alert('Error : your message was not sent');
+            });
+    };
+
+
+  }]);
+
   var user = {};
+
+  var inbox = [
+    {recipient: '0606666666', body: 'Ta mere xxxxx des xxxx en enfer !!!'},
+    {recipient: '0601111111', body: 'In vino veritas, mes freres !'}
+  ];
+
+  var outbox = [];
 
 })();
